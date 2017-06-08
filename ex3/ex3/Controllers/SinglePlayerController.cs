5d3bf9e1ff61080818ad78267f583c7edf9b3de6
shauli321 <shauli321@gmail.com>
2017-06-08 22:44:16 +0300

    minor changes (added the previous server as dll)

---
+++ ex3/ex3/Controllers/SinglePlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using MazeGeneratorLib;
+using MazeLib;
+using MazeServiceModel;
+using MazeServiceModel.Model;
+
+namespace ex3.Controllers
+{
+    public class SinglePlayerController : ApiController
+    {
+        IModel mazeModel;
+
+        // GET: api/SinglePlayer
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SinglePlayer/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SinglePlayer
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SinglePlayer/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SinglePlayer/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
