f96cddb56e558129c6bb812648b560882de992e0
shauli321 <shauli321@gmail.com>
2017-06-18 11:24:55 +0300

    started working on db; created first migration & users table

---
+++ ex3/ex3/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ex3.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+        public DateTime SigningDate { get; set; }
+        [Required]
+        public string UserName;
+        [Required]
+        public string EncryptedPassword;
+    }
+}
